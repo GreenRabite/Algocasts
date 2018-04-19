88d5b7cf4bb0f70ce4e786a9abb9c02e94a8939d
Aluo <green.rabite@gmail.com>
2018-04-18 19:39:18 -0700

    maxchar and reverseint

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let freq = {};
+  for (let i = 0; i < str.length; i++) {
+    if (freq[str[i]] === undefined) {
+      freq[str[i]]+=1;
+    }else {
+      freq[str[i]] = 1;
+    }
+  }
+  let max=0;
+  let result=str[0];
+  for(let ch in freq){
+    if (freq[ch] > max) {
+      max = freq[ch];
+      result = ch;
+    }
+  }
+  return result;
+}
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  if (n < 0) {
+    return -1 * parseInt(n.toString().split("").reverse().join(""));
+  }else {
+    return parseInt(n.toString().split("").reverse().join(""));
+  }
+}
+
 
 module.exports = reverseInt;
